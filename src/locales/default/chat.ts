443ddb4d435b937e6f3bd3dff12cf0e768ef4c07
Xifo Wu <xifo.wu@gmail.com>
2025-03-03 09:42:07 +0800

    chore: change chat name

--- src/locales/default/chat.ts
+++ src/locales/default/chat.ts
@@ -45,7 +45,7 @@ export default {
   historySummary: '历史消息总结',
   inbox: {
     desc: '开启大脑集群，激发思维火花。你的智能助理，在这里与你交流一切',
-    title: '随便聊聊',
+    title: 'AI 伙伴',
   },
   input: {
     addAi: '添加一条 AI 消息',
